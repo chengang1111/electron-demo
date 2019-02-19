aace79ce0d10ec6a3cbed5882f28115999b03d21
wb-cg449253 <wb-cg449253@alibaba-inc.com>
2019-02-19 20:50:56 +0800

    add main.js

--- main.js
+++ main.js
@@ -1,56 +1,74 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-function createWindow () {
-  // Create the browser window.
+import { app, BrowserWindow,Menu,MenuItem,dialog,ipcMain } from 'electron';
+import {appMenuTemplate} from './appmenu.js';
+let safeExit = false;
+let mainWindow;
+const createWindow = () =>{
   mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true
-    }
+    width:800,
+    height:600,
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+mainWindow.loadURL(`file://${__dirname}/index.html`);
+//增加主菜单
+const menu = Menu.buildFromTemplate(appMenuTemplate);//从模版创建主菜单；
+//在file菜单下添加名为New的子菜单
+menu.items[0].submenu.append(new MenuItem({
+  label:'New',
+  click(){
+    mainWindow.webContents.send('action','new');//点击后向主页面渲染进程发送‘新建文件’的命令
+  },
+  accelerator:'CmdOrCtrl+N'//快捷键
+}));
+//在New菜单后面添加名为Open的同级菜单
+menu.items[0].submenu.append(new MenuItem({
+  label:'Open',
+  click(){
+    mainWindow.webContents.send('action','open');//点击后向主页面渲染进程发送‘打开文件’的命令
+  },
+  accelerator:'CmdOrCtrl+O'
+}));
+menu.items[0].submenu.append(new MenuItem({
+  label:'Save',
+  click(){
+    mainWindow.webContents.send('action','save');//点击后向主页渲染进程发送‘保存’的命令
+  },
+  accelerator:'CmdOrCtrl+S'
+}))
+menu.items[0].submenu.append(new MenuItem({
+  type:'separator'
+}));
+menu.items[0].submenu.append(new MenuItem({
+  role:'quit'
+}));
+Menu.setApplicationMenu(menu);//注意：这个代码要放到菜单添加完成之后，否则会造成新增菜单快捷键的无效
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+mainWindow.on('close',(e) => {
+  if(!safeExit){
+    e.preventDefault();
+    mainWindow.webContents.send('action','exiting');
+  }
+});
+mainWindow.on('close',() => {
+  mainWindow = null;
+})
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+app.on('ready',createWindow);
+app.on('window-all-closed',() => {
+  if(process.platform !== 'darwin'){
+    app.quit();
+  }
+});
+app.on('activate',() => {
+  if(mainWindow === null) {
+    createWindow();
+  }
+});
+ipcMain.on('reqaction',(event,arg) => {
+  switch(arg){
+    case 'exit': 
+      safeExit=true;
+      app.quit();
+      break
   }
 })
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
